01f1698e68960404fb429586aa445e88068d2e2e
Santhosh0130 <bigilsangu@gmail.com>
2025-02-23 17:53:18 +0530

    After 13.1

--- src/main/java/com/example/home_rental_app1/controller/CartController.java
+++ src/main/java/com/example/home_rental_app1/controller/CartController.java
@@ -2,6 +2,7 @@ package com.example.home_rental_app1.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.home_rental_app1.dto.Cart;
 import com.example.home_rental_app1.service.CartService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/cart")
 public class CartController {
