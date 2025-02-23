dfd38fdb48f491d5fcec49ab6a644d396a1c2d65
Santhosh0130 <bigilsangu@gmail.com>
2025-02-23 15:29:42 +0530

    After 5

--- src/main/java/com/example/home_rental_app1/config/SecurityConfig.java
+++ src/main/java/com/example/home_rental_app1/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         // configuration.addAllowedOrigin("http://localhost:5173");
         // // configuration.setAllowedOrigins(orgins);
-        configuration.addAllowedOrigin("https://home-rental-app.onrender.com");
+        configuration.addAllowedOrigin("https://home-rental-app-frontend.onrender.com");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
         configuration.setAllowCredentials(true);
